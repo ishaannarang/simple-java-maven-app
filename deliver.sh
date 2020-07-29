1a88fd0c83c05f3a94d4ecc523baa2af2843b65b
goodfella511 <41102264+ishaannarang@users.noreply.github.com>
2020-07-30 01:05:07 +0530

    Create deliver.sh

---
+++ deliver.sh
@@ -0,0 +1,6 @@
+aws eks --region us-east-1 update-kubeconfig --name eks2
+kubectl apply -f my-app.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl autoscale deployment my-app --cpu-percent=50 --min=1 --max=10
+sleep 5
+kubectl get pods
