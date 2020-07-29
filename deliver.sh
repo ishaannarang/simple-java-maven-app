4575d7cfa7db48621328cc2ed43d92fbc7c2fd9e
goodfella511 <41102264+ishaannarang@users.noreply.github.com>
2020-07-30 02:17:30 +0530

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,5 +1,5 @@
 kubectl apply -f my-app.yaml
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
-kubectl autoscale deployment my-app --cpu-percent=50 --min=1 --max=10
+kubectl autoscale deployment my-deployment --cpu-percent=50 --min=1 --max=10
 sleep 5
 kubectl get pods
