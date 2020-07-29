7503323344cca7afe1781ab84ebbcc2b225607c4
goodfella511 <41102264+ishaannarang@users.noreply.github.com>
2020-07-30 01:32:04 +0530

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -1,4 +1,3 @@
-#/usr/local/bin/aws eks --region us-east-1 update-kubeconfig --name eks2
 kubectl apply -f my-app.yaml
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
 kubectl autoscale deployment my-app --cpu-percent=50 --min=1 --max=10
